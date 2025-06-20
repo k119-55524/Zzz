b0705ba4f81ef7c693878fdb27f43100e7df2183
Дмитрий <k119_55524@mail.ru>
2025-06-21 00:06:14 +0500

    Добавил инровой проект для виндовс

---
+++ Zzz/gameWin/gameWin.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "pch.h"
+#include "gameWin.h"
+
+int APIENTRY wWinMain(
+	_In_		HINSTANCE	hInstance,
+	_In_opt_	HINSTANCE	hPrevInstance,
+	_In_		LPWSTR		lpCmdLine,
+	_In_		int			nCmdShow)
+{
+	return (int)0;
+}
---
+++ Zzz/gameWin/pch.h
@@ -0,0 +1,12 @@
+﻿
+#ifndef PCH_H
+#define PCH_H
+
+// Добавьте сюда заголовочные файлы для предварительной компиляции
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN // Исключите редко используемые компоненты из заголовков Windows
+
+// Файлы заголовков Windows
+#include <windows.h>
+
+#endif //PCH_H
