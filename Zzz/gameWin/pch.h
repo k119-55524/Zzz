6c9c71980bf1c9d8847870edca4af0f624aaa841
Дмитрий <k119_55524@mail.ru>
2026-02-03 00:52:44 +0500

    Компилируется

--- Zzz/gameWin/pch.h
+++ Zzz/gameWin/pch.h
@@ -1,17 +1,17 @@
 ﻿
-#ifndef PCH_H
-#define PCH_H
+#pragma once
 
 #include <string>
 #include <locale>
+#include <sstream>
 #include <codecvt>
 #include <iostream>
 #include <exception>
+#include <stdexcept>
+#include <source_location>
 
 #include <SDKDDKVer.h>
 #define WIN32_LEAN_AND_MEAN // Исключите редко используемые компоненты из заголовков Windows
 
 // Файлы заголовков Windows
 #include <windows.h>
-
-#endif //PCH_H
