b6a2bab682fd3eabde0c3e142bb7df04e054f61b
Дмитрий <k119_55524@mail.ru>
2026-02-08 02:17:25 +0500

    engineBase компилируется под Vulkan

--- Zzz/engineFoundation/engineBase/headers/HeaderVulkan.h
+++ Zzz/engineFoundation/engineBase/headers/HeaderVulkan.h
@@ -1,8 +1,15 @@
+
 #pragma once
 
 #if defined(ZRENDER_API_VULKAN)
+
+// ---- Vulkan ----
+#define VK_NO_PROTOTYPES
 #include <vulkan/vulkan.h>
+
+// ---- GLM ----
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_LEFT_HANDED
 #include <glm/glm.hpp>
+
 #endif // ZRENDER_API_VULKAN
