5f7a76606800d545f4953819800e341d9859d0ed
Dmitriy Zakharov <k119_55524@mail.ru>
2026-05-20 02:52:00 +0500

    Андроид проект собирается

--- projects/game_android/src/android_main.cpp
+++ projects/game_android/src/android_main.cpp
@@ -26,7 +26,7 @@ void android_main(struct android_app* app)
 		int events;
 		struct android_poll_source* source;
 
-		while ((ident = ALooper_pollAll(0, nullptr, &events, (void**)&source)) >= 0)
+		while ((ident = ALooper_pollOnce(0, nullptr, &events, (void**)&source)) >= 0)
 		{
 			if (source != nullptr)
 			{
