1cdc859ffd3caade3d8dc5b9a708f7b9552f4c90
Dmitriy Zakharov <k119_55524@mail.ru>
2026-06-09 03:10:27 +0500

    Дофикс

--- src/engine/platform_types.h
+++ src/engine/platform_types.h
@@ -2,16 +2,12 @@
 
 #if defined(Z_ANDROID)
 struct android_app;
-#elif defined(Z_MACOS) || defined(Z_IOS)
-// Apple specific forward declarations
 #endif
 
 namespace zzz::engine
 {
 #if defined(Z_ANDROID)
 	using PlatformNativeData = android_app;
-#elif defined(Z_MACOS) || defined(Z_IOS)
-	using PlatformNativeData = void;
 #else
 	using PlatformNativeData = void;
 #endif
