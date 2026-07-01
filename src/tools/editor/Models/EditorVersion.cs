6e88887fee9028f6c4fc48d05fde76d581bdf706
Dmitriy Zakharov <k119_55524@mail.ru>
2026-07-02 01:24:05 +0500

    Правки юай

--- src/tools/editor/Models/EditorVersion.cs
+++ src/tools/editor/Models/EditorVersion.cs
@@ -2,7 +2,7 @@ namespace editor;
 public static class EditorConstants
 {
 public const string ApplicationName = "ZzzEditor";
-public const string Version = "1.0.0:27986";
+public const string Version = "1.0.0:27994";
 public const int VersionCode = 1;
-public const string BuildDate = "02.07.2026 00:00:12";
+public const string BuildDate = "02.07.2026 01:22:40";
 }
