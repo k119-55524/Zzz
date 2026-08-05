8b2dd873ce320c6af00efc8815e6dff5dfb22c8d
Dmitriy Zakharov <k119_55524@mail.ru>
2026-08-05 14:34:41 +0500

    Дофикс

--- src/tools/editor/Models/EditorVersion.cs
+++ src/tools/editor/Models/EditorVersion.cs
@@ -2,7 +2,7 @@ namespace editor;
 public static class EditorConstants
 {
 public const string ApplicationName = "ZzzEditor";
-public const string Version = "1.0.0:32894";
+public const string Version = "1.0.0:32968";
 public const int VersionCode = 1;
-public const string BuildDate = "05.08.2026 02:03:54";
+public const string BuildDate = "05.08.2026 14:28:30";
 }
